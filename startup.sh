1bc8f6398c30e3d9c52422f4e6eab120df115174
Matt McNamee <matt@mcnamee.co>
2020-06-29 14:56:39 +1000

    Missed intro from stash

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 # Print a welcome message
 echo -e ' \n '
@@ -9,6 +11,11 @@ echo -e '   | |_) / _ \ `_ \| |/ _ \/ __| __|   | |/ _ \ / _ \| | |/ / | __| '
 echo -e '   |  __/  __/ | | | |  __/\__ \ |_    | | (_) | (_) | |   <| | |_  '
 echo -e '   |_|   \___|_| |_|_|\___||___/\__|   |_|\___/ \___/|_|_|\_\_|\__| '
 echo -e '////////////////////////////////////////////////////////////////////////';
+echo -e '               Built and maintained by Matt Mcnamee'
+echo -e '            https://github.com/mcnamee/pentest-toolkit'
+echo -e "${bold}"
+echo -e '     Recon  —  Exploitation  —  Privilege Escalation  —  Persistence'
+echo -e "${normal}"
 
 # Ensure the final executable receives the Unix signals
 exec "$@"
